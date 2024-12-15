85ab2fae95d0570929138ebf4b111a26949cbe15
shailesh kale <shaileshkale87730@gmail.com>
2024-12-15 18:38:24 +0530

    adding the resize of login

--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -50,7 +50,7 @@ const Singup = () => {
   };
   return (
     <div className="min-h-[83.9vh]  flex justify-center items-center">
-      <div className="  md:w-2/4 w-[88.333333%] h-[74.666667%]  bg-transparent  border  border-solid border-black rounded-lg flex items-center justify-center flex-col gap-2">
+      <div className="xl:w-[35%] lg:w-[32%]  md:w-2/4 w-[88.333333%] h-[74.666667%]  bg-transparent  border  border-solid border-black rounded-lg flex items-center justify-center flex-col gap-2">
         <Heading
           heading="Sing up"
           heading_type="h1"
